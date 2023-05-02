e9ef520c143e73661a52c38f2f3081e96eb5c374
V_Borysenko <preinspiron@gmail.com>
2023-05-02 17:55:46 +0200

    props

--- src/components/Feedback/Statistics.jsx
+++ src/components/Feedback/Statistics.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import { Stats } from './Feedback.styled';
 
 export const Statistics = ({ good, neutral, bad, positivePercentage }) => {
@@ -13,4 +14,10 @@ export const Statistics = ({ good, neutral, bad, positivePercentage }) => {
       </ul>
     </Stats>
   );
+};
+Statistics.propTypes = {
+  good: PropTypes.number,
+  neutral: PropTypes.number,
+  bad: PropTypes.number,
+  positivePercentage: PropTypes.func,
 };
